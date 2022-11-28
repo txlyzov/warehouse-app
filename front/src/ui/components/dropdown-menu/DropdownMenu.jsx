eab0ddbed19f0c1d761a0e10c224d21661453894
Timofey Khlyzov <t.khlyzov@syberry.com>
2022-11-28 19:23:33 +0300

    518258: Add dropdown for settings button option

---
+++ front/src/ui/components/dropdown-menu/DropdownMenu.jsx
@@ -0,0 +1,36 @@
+import "./DropdownMenu.scss"
+import React, { useState } from 'react';
+
+
+function DropdownMenu({ className, triggerContent, dropdownContent, width = "auto", position = {
+    top: "auto",
+    left: "auto",
+} }) {
+
+    const [dropdownVisability, setDropdownVisability] = useState(false);
+
+    return (
+        <div className={`dropdown-menu wrapper ${className}`}>
+            {triggerContent ?
+                <div
+                    aria-hidden="true"
+                    className="dropdown-menu__trigger"
+                    onClick={() => {
+                        setDropdownVisability(!dropdownVisability);
+                    }}
+                >
+                    {triggerContent}
+                </div>
+                :
+                null
+            }
+
+            {dropdownVisability ?
+                <div className="dropdown-menu__content" style={{ width, top: position.top, left: position.left }}>
+                    {dropdownContent}
+                </div> : null}
+        </div>
+    );
+}
+
+export default DropdownMenu;
--- front/src/ui/components/header/Header.jsx
+++ front/src/ui/components/header/Header.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import Icon from '../icon/Icon';
 import Button from '../button/Button';
 import { getLoginData, removeLoginData, setLoginData } from '../../../utils/LocalStorageUtil';
+import DropdownMenu from '../dropdown-menu/DropdownMenu';
 
 function Header() {
   const navigate = useNavigate();
@@ -20,6 +21,13 @@ function Header() {
     }
   };
 
+  // eslint-disable-next-line arrow-body-style
+  // const dropdownTrigger = () => {
+  //   return <Link className="login-block__username">{getLoginData().username}</Link>
+  // }
+
+  // const dropdownTrigger = () => <Link className="login-block__username">{getLoginData().username}</Link>
+
   return (
     <div className="header-wrapper">
       <div className="logo-block header-block">
@@ -32,10 +40,33 @@ function Header() {
         <Link className="options-block__option" to="/help">Help</Link>
         <Link onClick={LIT} className="options-block__option">[!LIT!]</Link>
       </div>
-
+      {/* <Link className="login-block__username">{getLoginData().username}</Link> */}
       <div className="login-block header-block">
         {getLoginData()
-          ? <Link className="login-block__username">{getLoginData().username}</Link>
+          ?
+          // <Link className="login-block__username">{getLoginData().username}</Link>
+          <DropdownMenu
+            // triggerContent={<Link className="login-block__username">{getLoginData().username}</Link>}
+            // triggerContent={function name() {
+            //   return getLoginData().username;
+            // }}
+
+            className="login-block__dropdown-menu"
+            width='250px'
+            position={
+              {
+                top: "55px",
+                left: "-20px"
+              }
+            }
+            triggerContent={<Link className="login-block__username">{getLoginData().username}</Link>}
+            dropdownContent={
+              <div className='login-block__dropdown-content'>
+                <Link className="login-block__dropdown-option" to="/settings">Settings</Link>
+                <Link onClick={() => { console.log(222) }} className="login-block__dropdown-option">Unlogin</Link>
+              </div>
+            }
+          />
           : (
             <div className="login-block__unlogin">
               <Button click={() => routeChange('sign-in')} text="Sign in" type="primary" size="sm" />
